9944041fea50669a5454cfa20525666eb66e276d
anglekto <47034952+anglekto@users.noreply.github.com>
2019-06-13 14:39:04 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,49 @@ function navBtn() {
     x.className = "navBtn";
   }
 }
+
+medSchoolCost = [
+  {
+    "label": "Public Medical School, In-State",
+    "cost": 34592,
+		"color":"red"
+  },
+  {
+    label: "Public Medical School, Out-of-State",
+    cost: 58668,
+		color:"blue"
+  },
+  {
+    label: "Private Medical School",
+    cost: 50000
+  },
+  {
+    label: "CAM Education",
+    cost: 25000
+  }
+];
+
+
+function barTemplate(data) {
+  return `
+  <div class="bar" >
+  ${data.label}, ${data.cost}
+  </div>
+  `
+}
+
+
+document.querySelector(".right").innerHTML = `
+<h2>Average Annual Cost of Tuition, 2016-2017</h2>
+<div class="barGraph">
+${medSchoolCost.map(barTemplate).join('')}
+</div>
+<div id="scale">
+<div class="scale"><p>$10k</p></div>
+<div class="scale"><p>$20k</p></div>
+<div class="scale"><p>$30k</p></div>
+<div class="scale"><p>$40k</p></div>
+<div class="scale"><p>$50k</p></div>
+<div class="scale"><p>$60k</p></div>
+</div>
+`;
